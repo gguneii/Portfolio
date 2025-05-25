dbe1059bb69b9639015445f8ddd1dd083753e2fb
gguneii <guneysuleymanofa@gmail.com>
2025-05-25 12:50:01 +0400

    added navbar

---
+++ src/app/components/header/Navbar.tsx
@@ -0,0 +1,110 @@
+"use client";
+import {
+  SunIcon,
+  MoonIcon,
+  XMarkIcon,
+  Bars3Icon,
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+const Navbar = () => {
+  const theme = "dark";
+  const [isMobileMenuOpen, setMobileMenuOpen] = useState<boolean>(false);
+  const pathname = usePathname();
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!isMobileMenuOpen);
+  };
+  const menuItems = [
+    { href: "/", label: "Home" },
+    { href: "/about", label: "About" },
+    { href: "/projects", label: "Projects" },
+    { href: "/blogs", label: "Blogs" },
+    { href: "/contact", label: "Contact" },
+  ];
+
+  return (
+    <nav className="fixed w-full bg-dark/80 backdrop-blur-sm z-50">
+      <div className="container max-w-7xl mx-auto px-4">
+        {/* Desktop Menu */}
+        <div className="flex items-center justify-between h-16">
+          <Link href="/" className="text-xl font-bold text-primary">
+            Portfolio&trade;
+          </Link>
+
+          {/* desktop menus  */}
+          <div className="hidden md:flex items-center space-x-8">
+            {menuItems.map((item) => {
+              const isActive = pathname === item.href;
+              return (
+                <Link
+                  key={item.href}
+                  href={item.href}
+                  className={`hover:text-[#007AFF] transition-colors font-medium ${
+                    isActive ? "text-primary" : ""
+                  }`}
+                >
+                  {item.label}
+                </Link>
+              );
+            })}
+
+            <button className="p-2 rounded-lg hover:bg-gray-100 text-white hover:text-primary dark:hover:bg-gray-800 transition-colors cursor-pointer">
+              {theme === "dark" ? (
+                <SunIcon className="w-5 h-5" />
+              ) : (
+                <MoonIcon className="w-5 h-5" />
+              )}
+            </button>
+          </div>
+
+          {/* mobile menu button  */}
+
+          <button
+            className="md:hidden p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 transition-colors cursor-pointer"
+            onClick={toggleMobileMenu}
+          >
+            {isMobileMenuOpen ? (
+              <XMarkIcon className="w-6 h-6" />
+            ) : (
+              <Bars3Icon className="w-6 h-6" />
+            )}
+          </button>
+        </div>
+
+        {/* Mobile Menu */}
+        {isMobileMenuOpen && (
+          <div className="md:hidden">
+            <div className="py-4 space-y-4">
+              {menuItems.map((item, index) => {
+                return (
+                  <div key={index}>
+                    <Link onClick={toggleMobileMenu}
+                      href={item.href}
+                      className="block py-2 hover:text-primary transition-colors"
+                    >
+                      {item.label}
+                    </Link>
+                  </div>
+                );
+              })}
+
+              <div>
+                <button className="flex items-center py-2 hover:text-primary transition-colors cursor-pointer">
+                  {theme === "dark" ? (
+                    <><SunIcon className="w-5 h-5 mr-2" />Light Mode</>
+                  ) : (
+                    <><MoonIcon className="w-5 h-5 mr-2" />Dark Mode</>
+                  )}
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
